8b439a959aafec3f3c0b1e365bbccc02b33e5a62
scott5Tots <85150375+scott5Tots@users.noreply.github.com>
2022-10-26 21:40:58 -0400

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -3,11 +3,11 @@
 #Date: 10/26/2022
 #Description: program takes two positive integers and "multiplies" them by adding.
 
-def mult(mul1, mul2):
+def multiply(mul1, mul2):
     '''function takes two integers and multiplies them.'''
     if(mul1 == 0):
         #stops sequence once num1 reaches 0
         return 0
     else:
         #adds mul2 to decresing integers of mul1
-        return mul2+mult(mul1-1, mul2)
+        return mul2+multiply(mul1-1, mul2)
